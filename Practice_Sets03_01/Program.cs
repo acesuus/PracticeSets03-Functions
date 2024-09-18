398acf0ef61bd82253f7f36a15e17915db4c0b48
Kert-Ace T. Ombion <155365666+acesuus@users.noreply.github.com>
2024-09-18 12:42:58 +0800

    Done

---
+++ Practice_Sets03_01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP.PracticeSets03
+{
+    class AreaofTriangle
+    {
+        double Heron_Formula(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+            double Area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return Area;
+        }
+    }
+}
---
+++ Practice_Sets03_02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP.PracticeSets03
+{
+    class Year
+    {
+        static bool IsYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0 && year % 400 != 0)
+                {
+                    return false;
+                }
+                return true;
+                
+
+            }
+            return false;
+        }
+    }
+}
